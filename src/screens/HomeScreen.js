5f44255fc3a6ebe541c8527338d24c2a2febc327
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2025-10-18 12:42:01 +0800

    Add Categories and FoodItems components to HomeScreen with props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -762,9 +762,17 @@ export default function HomeScreen() {
           </Text>
         </View>
 
-        <View testID="categoryList"></View>
+        <View testID="categoryList">
+          <Categories
+            categories={categories}
+            activeCategory={activeCategory}
+            handleChangeCategory={handleChangeCategory}
+          />
+        </View>
 
-        <View testID="foodList"></View>
+        <View testID="foodList">
+          <FoodItems foods={filteredfoods} categories={categories} />
+        </View>
       </ScrollView>
     </View>
   );
